64f36b33e2a7170b7b17fb377628704d2b219977
Mason Wolf <masonhwolf@gmail.com>
2019-04-02 19:40:28 -0500

    Added FileIO example

--- Apps/FileIOExample/src/main/FileIOExample.java
+++ Apps/FileIOExample/src/main/FileIOExample.java
@@ -1,10 +1,48 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
 public class FileIOExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	private static BufferedReader fileReader;
+	
+	public static void main(String[] args) throws IOException {
+		
+		String file = "students.txt";
+		Paths.get(file);
+		
+		fileReader = new BufferedReader(new FileReader(file));
+		String line = fileReader.readLine();
+		
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		
+		while (line != null)
+		{
+			String[] studentData = line.split("#");
+			
+			Student student = new Student(studentData[1], Double.parseDouble(studentData[2]));
+			studentList.add(student);
+			line = fileReader.readLine();
+		}
+		
+		fileReader.close();
+		
+		for (Student student : studentList)
+		{
+			System.out.println(student.getName() + "\t" + student.getGrade());
+		}
 	}
+
 
 }
---
+++ Apps/FileIOExample/src/main/Student.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Student {
+
+	private String name;
+	private double grade;
+	
+	public Student(String name, double grade)
+	{
+		this.name = name;
+		this.grade = grade;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getGrade()
+	{
+		return grade;
+	}
+	
+	public void setGrade(double grade)
+	{
+		this.grade = grade;
+	}
+}
